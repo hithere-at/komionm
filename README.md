0c936503b9bdbe9c381b61eb1fc129224053e5be
bestpeople105 <yurifadiila@gmail.com>
2022-02-20 19:08:59 +0700

    Deploy

---
+++ README.md
@@ -0,0 +1,17 @@
+# komionm
+This is a Python script to download said `Komi Can't Communicate chapters`. This code is messy, idk why i use `asyncio` and `aiohttp`.
+
+## Requirements
+1. [Python](https://www.python.org)
+2. xdg-open
+
+## How To Install
+```
+git clone https://github.com/bestpeople105/komionm.git && cd komionm
+sh install.sh
+```
+
+## Python Dependencies
+1. asyncio
+2. beautifulsoup4
+3. aiohttp
---
+++ install.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+gnu_lnx="~/komionm/chapters/"
+termux="/storage/emulated/0/komionm/chapters/"
+
+echo "Installing python packages..."
+pip3 install -r requirements.txt
+
+echo "1. GNU/Linux\n2. Android (Termux)"
+
+while true; do
+	echo -n "Please choose the platform that you currently use: "
+	read choices
+
+	if [ $choices -eq 1 ]; then
+		mkdir ~/komionm
+		mkdir ~/komionm/chapters
+		sudo cp komionm /usr/local/bin/komionm
+		sed -i "s|chapters/|$gnu_lnx|g" /usr/local/bin/komionm
+		echo "Successfully installed\nLocation: $gnu_lnx"
+		break
+
+	elif [ $choices -eq 2 ]; then
+		mkdir /storage/emulated/0/komionm
+		mkdir /storage/emulated/0/komionm/chapters
+		cp komionm /data/data/com.termux/files/usr/bin/komionm
+		sed -i "s|chapters/|$termux|g" /data/data/com.termux/files/usr/bin/komionm
+		echo "Successfully installed\nLocation: $termux"
+		break
+
+	else
+		echo "No such option available. Try again"
+
+	fi
+done
