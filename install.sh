721f5f9425d5779fe61d431fff8d7c42912126af
bestpeople105 <yurifadiila@gmail.com>
2022-03-04 13:51:05 +0700

    installer update

--- install.sh
+++ install.sh
@@ -3,10 +3,19 @@
 gnu_lnx="~/komionm/chapters/"
 termux="/storage/emulated/0/komionm/chapters/"
 
-if ! $(command -v python3 > /dev/null 2>&1); then "Python 3 isnt installed. Install it using your package manager"; exit 1
+if ! $(command -v tac > /dev/null 2>&1); then "tac isnt installed. Install it using your package manager"; exit 1
 fi
 
 if ! $(command -v curl > /dev/null 2>&1); then "curl isnt installed. Install it using your package manager"; exit 1
+fi
+
+if ! $(command -v gawk > /dev/null 2>&1); then "gawk isnt installed. Install it using your package manager"; exit 1
+fi
+
+if ! $(command -v sed > /dev/null 2>&1); then "sed isnt installed. Install it using your package manager"; exit 1
+fi
+
+if ! $(command -v grep > /dev/null 2>&1); then "grep isnt installed. Install it using your package manager"; exit 1
 fi
 
 echo "\n1. GNU/Linux\n2. Android (Termux)"
@@ -26,7 +35,7 @@ while true; do
 		mkdir ~/komionm/chapters
 		mkdir ~/komionm/chapters/scans
 		sudo cp komionm /usr/local/bin/komionm
-		sed -i "s|data_path=\".*\"|data_path=\"$gnu_lnx\"|" /usr/local/bin/komionm
+		sed -i "s|data_path=.*|data_path=$gnu_lnx|" /usr/local/bin/komionm
 		echo "Successfully installed\nChapters download location: $gnu_lnx"
 		break
 
@@ -36,7 +45,7 @@ while true; do
 		mkdir /storage/emulated/0/komionm/chapters
 		mkdir /storage/emulated/0/komionm/chapters/scans
 		cp komionm /data/data/com.termux/files/usr/bin/komionm
-		sed -i "s|data_path = \".*\"|data_path=\"$termux\"|" /data/data/com.termux/files/usr/bin/komionm
+		sed -i "s|data_path=.*|data_path=$termux|" /data/data/com.termux/files/usr/bin/komionm
 		sed -i "s|#\!/bin/sh|#!/data/data/com.termux/files/usr/bin/sh|" /data/data/com.termux/files/usr/bin/komionm
 		echo "Successfully installed\nChapters download location: $termux"
 		break
